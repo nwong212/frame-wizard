69ba01a4188ac6450e0a76bb5798846eee0e79c1
Nathan <nwong@risd.edu>
2020-06-29 13:11:46 -0400

    fixed math

--- source/wizard.js
+++ source/wizard.js
@@ -51,160 +51,23 @@ function frameDesign() {
 	var remainder = document.getElementById('remaining');
 	var svgRemainder = document.getElementById('measurement-remainder');
 
-//When Rabbet Changes
-	rabbet.addEventListener("input", rabbetChanged);
-
-	function rabbetChanged() {
-		document.getElementById("measurement-rabbet").textContent = rabbet.value + "\42";
-	}
-
-//When Spacer Changes
-	spacer.addEventListener("input", spacerd);
-
-	function spacerd() {
-
-		document.getElementById("measurement-spacer").textContent = spacer.value + "\42";
-
-		if (spacer.value.length == 0 || spacer.value == 0) {
-
-			spacer.value = 0
-
-			document.getElementById("spacer-group").style.display = "none";
-		} else {
-
-			document.getElementById("spacer-group").style.display = "inline";
-		}
-	}
-
-	//Top Mat
-
-	topSelect.addEventListener("change", topChange, false);
-
-	function topChange() {
-
-		document.getElementById("measurement-top").textContent = topSelect.options[topSelect.selectedIndex].value + "\42";
-
-		if (topSelect.value == "1/16") {
-			document.getElementById("top-group").style.display = "inline";
-
-			document.getElementById("svg-top").setAttribute("points", "103.5,61.3 40.7,61.3 40.7,65.3 109.2,65.3 ");
-
-		} else if (topSelect.value == "1/8") {
-			document.getElementById("top-group").style.display = "inline";
-
-			document.getElementById("svg-top").setAttribute("points", "103.5,61.3 40.7,61.3 40.7,69.3 109.2,69.3");
-
-		} else if (topSelect.value == "0") {
-			document.getElementById("top-group").style.display = "none";
-		}
-	}
-
-	//Bottom Mat
-
-	bottomSelect.addEventListener("change", bottomChange, false);
-
-	function bottomChange() {
-
-		document.getElementById("measurement-bottom").textContent = bottomSelect.options[bottomSelect.selectedIndex].value + "\42";
-
-		if (bottomSelect.value == "1/16") {
-
-			document.getElementById("svg-bottom").setAttribute("height", "4");
-
-			document.getElementById("svg-bottom").style.display = "inline";
-			document.getElementById("measurement-bottom").style.display = "inline";
-
-		} else if (bottomSelect.value == "1/8") {
-
-			document.getElementById("svg-bottom").setAttribute("height", "8");
-
-			document.getElementById("svg-bottom").style.display = "inline";
-			document.getElementById("measurement-bottom").style.display = "inline";
-
-		} else if (bottomSelect.value == "0") {
-			document.getElementById("svg-bottom").style.display = "none";
-			document.getElementById("measurement-bottom").style.display = "none";
-		}
-	}
-
-	//Backing Board
-
-	backingBoard.addEventListener("input", backingChanged);
-
-	function backingChanged() {
-		document.getElementById("measurement-backing").textContent = backingBoard.value + "\42";
-
-		if (backingBoard.value.length == 0 || backingBoard.value == "0") {
-			document.getElementById("svg-board").style.display = "none";
-			document.getElementById("measurement-backing").style.display = "none";
-
-			backingBoard.value = 0
-
-		} else {
-			document.getElementById("svg-board").style.display = "inline";
-			document.getElementById("measurement-backing").style.display = "inline";
-		}
-
-	}
-
-	// Additional Space
-	additional.addEventListener("input", moreAdded);
-
-	function moreAdded() {
-		document.getElementById("measurement-additional").textContent = additional.value + "\42";
-
-		if (additional.value.length == 0 || additional.value == 0) {
-			document.getElementById("additional-group").style.display = "none";
-			additional.value = 0
-		} else {
-
-		document.getElementById("additional-group").style.display = "inline";
-		}
-	}
-
-
-	//Strainer
-	strainer.addEventListener("input", strainerChanged);
-
-	function strainerChanged() {
-		document.getElementById("measurement-strainer").textContent = strainer.value + "\42";
-
-		if (strainer.value.length == 0 || strainer.value == "0") {
-			document.getElementById("svg-strainer").style.display = "none";
-			document.getElementById("measurement-strainer").style.display = "none";
-			strainer.value = 0
-		} else {
-			document.getElementById("svg-strainer").style.display = "inline";
-			document.getElementById("measurement-strainer").style.display = "inline";
-		}
-
-	}
-
-	//Glazing
-	glazing.addEventListener("input", glazingChanged);
-
-	function glazingChanged() {
-		document.getElementById("measurement-glazing").textContent = glazing.value + "\42";
-
-		if (glazing.value == "0" || glazing.length == 0) {
-			document.getElementById("svg-glazing").style.display = "none";
-			document.getElementById("measurement-glazing").style.display = "none";
-			glazing.value = 0;
-		} else {
-			document.getElementById("svg-glazing").style.display = "inline";
-			document.getElementById("measurement-glazing").style.display = "inline";
-		}
-
-
-
-	}
-
 	//Calculator
 	//This function calculates the difference between the rabbet depth and the package depth and outputs a user-friendly fraction.
 
 	form.addEventListener('change', calculate);
 
 	function calculate() {
+
+		//Reset any blank fields to 0
+
+		var fields = document.getElementsByClassName("values");
+
+		Array.from(fields, function(el){
+
+			if (el.value.length == 0) {
+				el.value = 0;
+			}
+		});
 
 		//defining our values list
 
@@ -278,10 +141,6 @@ function frameDesign() {
 		topEl.setAttribute("height", topHeight);
 
 		document.getElementById("notTop").setAttribute("transform", "translate(0 " + topDiff + ")");
-
-
-
-		// topEl.setAttribute("height", spacerHeight)
 
 		//Bottom Mat
 		var bottomHeight = scaledElements[4];
@@ -358,6 +217,156 @@ function frameDesign() {
 		//
 		// }
 	}
+
+
+
+//When Rabbet Changes
+	rabbet.addEventListener("input", rabbetChanged);
+
+	function rabbetChanged() {
+		document.getElementById("measurement-rabbet").textContent = rabbet.value + "\42";
+	}
+
+//When Spacer Changes
+	spacer.addEventListener("input", spacerd);
+
+	function spacerd() {
+
+		document.getElementById("measurement-spacer").textContent = spacer.value + "\42";
+
+		if (spacer.value.length == 0 || spacer.value == 0 || spacer.value == "") {
+
+			document.getElementById("spacer-group").style.display = "none";
+
+		} else {
+
+			document.getElementById("spacer-group").style.display = "inline";
+		}
+	}
+
+	//Top Mat
+
+	topSelect.addEventListener("change", topChange, false);
+
+	function topChange() {
+
+		document.getElementById("measurement-top").textContent = topSelect.options[topSelect.selectedIndex].value + "\42";
+
+		if (topSelect.value == "1/16") {
+			document.getElementById("top-group").style.display = "inline";
+
+			document.getElementById("svg-top").setAttribute("points", "103.5,61.3 40.7,61.3 40.7,65.3 109.2,65.3 ");
+
+		} else if (topSelect.value == "1/8") {
+			document.getElementById("top-group").style.display = "inline";
+
+			document.getElementById("svg-top").setAttribute("points", "103.5,61.3 40.7,61.3 40.7,69.3 109.2,69.3");
+
+		} else if (topSelect.value == "0") {
+			document.getElementById("top-group").style.display = "none";
+		}
+	}
+
+	//Bottom Mat
+
+	bottomSelect.addEventListener("change", bottomChange, false);
+
+	function bottomChange() {
+
+		document.getElementById("measurement-bottom").textContent = bottomSelect.options[bottomSelect.selectedIndex].value + "\42";
+
+		if (bottomSelect.value == "1/16") {
+
+			document.getElementById("svg-bottom").setAttribute("height", "4");
+
+			document.getElementById("svg-bottom").style.display = "inline";
+			document.getElementById("measurement-bottom").style.display = "inline";
+
+		} else if (bottomSelect.value == "1/8") {
+
+			document.getElementById("svg-bottom").setAttribute("height", "8");
+
+			document.getElementById("svg-bottom").style.display = "inline";
+			document.getElementById("measurement-bottom").style.display = "inline";
+
+		} else if (bottomSelect.value == "0") {
+			document.getElementById("svg-bottom").style.display = "none";
+			document.getElementById("measurement-bottom").style.display = "none";
+		}
+	}
+
+	//Backing Board
+
+	backingBoard.addEventListener("input", backingChanged);
+
+	function backingChanged() {
+		document.getElementById("measurement-backing").textContent = backingBoard.value + "\42";
+
+		if (backingBoard.value.length == 0 || backingBoard.value == "0" || backingBoard.value =="") {
+			document.getElementById("svg-board").style.display = "none";
+			document.getElementById("measurement-backing").style.display = "none";
+
+		} else {
+			document.getElementById("svg-board").style.display = "inline";
+			document.getElementById("measurement-backing").style.display = "inline";
+		}
+
+	}
+
+	// Additional Space
+	additional.addEventListener("input", moreAdded);
+
+	function moreAdded() {
+		document.getElementById("measurement-additional").textContent = additional.value + "\42";
+
+		if (additional.value.length == 0 || additional.value == 0 ||additional.value =="") {
+			document.getElementById("additional-group").style.display = "none";
+		} else {
+
+		document.getElementById("additional-group").style.display = "inline";
+		}
+	}
+
+
+	//Strainer
+	strainer.addEventListener("input", strainerChanged);
+
+	function strainerChanged() {
+		document.getElementById("measurement-strainer").textContent = strainer.value + "\42";
+
+		if (strainer.value.length == 0 || strainer.value == "0" ||strainer.value =="") {
+			document.getElementById("svg-strainer").style.display = "none";
+			document.getElementById("measurement-strainer").style.display = "none";
+		} else {
+			document.getElementById("svg-strainer").style.display = "inline";
+			document.getElementById("measurement-strainer").style.display = "inline";
+		}
+
+	}
+
+	//Glazing
+	glazing.addEventListener("input", glazingChanged);
+
+	function glazingChanged() {
+		document.getElementById("measurement-glazing").textContent = glazing.value + "\42";
+
+		if (glazing.value == "0" || glazing.length == 0 || glazing.value == "") {
+
+			document.getElementById("svg-glazing").style.display = "none";
+
+			document.getElementById("measurement-glazing").style.display = "none";
+
+		} else {
+
+			document.getElementById("svg-glazing").style.display = "inline";
+
+			document.getElementById("measurement-glazing").style.display = "inline";
+		}
+
+
+
+	}
+
 	/**
 	 * Converts numbers to fractions:
 	 * - 1.25 to 1 1/4
