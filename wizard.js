0872c18cfb2f1461ab3afc3486352d975bb128fa
Nathan <nwong@risd.edu>
2020-07-01 00:39:16 -0400

    simplified code a little bit.

--- wizard.js
+++ wizard.js
@@ -1,1 +1,326 @@
-function eventWindowLoaded(){frameDesign(),downloadSVG(),setYPosition()}function frameDesign(){var e=document.getElementById("rabbet"),t=document.getElementById("frameside"),n=document.getElementById("spacer"),a=document.getElementById("svg-spacer"),l=document.getElementById("topMat"),o=document.getElementById("svg-top"),d=document.getElementById("bottomMat"),m=document.getElementById("svg-bottom"),i=document.getElementById("artwork"),u=document.getElementById("svg-artwork"),s=document.getElementById("backing"),r=document.getElementById("svg-board"),g=document.getElementById("additional"),c=document.getElementById("svg-additional"),y=document.getElementById("strainer"),v=document.getElementById("svg-strainer"),E=document.getElementById("svg-x"),p=document.getElementById("glazing"),I=document.getElementById("svg-glazing"),B=document.getElementById("frameCalc"),b=document.getElementById("remaining"),f=document.getElementById("measurement-remainder");B.addEventListener("change",(function(){var B=document.getElementsByClassName("values");Array.from(B,(function(e){0==e.value.length&&(e.value=0)}));var A=[e.value,p.value,n.value,l.value,d.value,s.value,y.value,g.value,i.value];A.forEach((function(e,t,n){if(-1!=e.indexOf("/")){var a,l=e.split(" ");l.length>1?a=l[1].split("/"):(a=l[0].split("/"),l[0]=0),n[t]=parseInt(l[0],10)+parseInt(a[0],10)/parseInt(a[1],10)}else n[t]=parseInt(e,10)}));var w=A.slice(1),L=A[0]-w.reduce((e,t)=>e+t,0),x=A.map((function(e){return 64*e})),k=x[0];t.setAttribute("height",k+10);var C=x[1],z=C-8;I.setAttribute("height",C),document.getElementById("notGlazing").setAttribute("transform","translate(0 "+z+")");var M=x[2],S=M-40;a.setAttribute("height",M),document.getElementById("notSpacer").setAttribute("transform","translate(0 "+S+")");var R=x[3],U=R-4;o.setAttribute("height",R),document.getElementById("notTop").setAttribute("transform","translate(0 "+U+")");var D=x[4],F=D-4;m.setAttribute("height",D),document.getElementById("notBottom").setAttribute("transform","translate(0 "+F+")");var G=x[8],O=G;u.setAttribute("height",G),document.getElementById("notArtwork").setAttribute("transform","translate(0 "+O+")");var P=x[5],Y=P-8;r.setAttribute("height",P),document.getElementById("notBacking").setAttribute("transform","translate(0 "+Y+")");var j=x[6],N=j-32+128.5;v.setAttribute("height",j),E.setAttribute("y2",N);var T,V,W=x[7];c.setAttribute("height",W),document.getElementById("strainer-group").setAttribute("transform","translate(0 "+W+")"),b.value=h(L)+'"',f.textContent=h(L)+'"',L<0?(V=Math.abs(L),T=h(V),b.value="-"+T+'"',f.textContent="-"+T+'"',b.classList.add("negative"),f.setAttribute("class","negative")):L>=0&&(T=h(L),b.value=T+'"',f.textContent=T+'"',b.classList.remove("negative"),f.setAttribute("class","positive"));setYPosition()})),e.addEventListener("input",(function(){document.getElementById("measurement-rabbet").textContent=e.value+'"'})),n.addEventListener("input",(function(){document.getElementById("measurement-spacer").textContent=n.value+'"',0==n.value.length||0==n.value||""==n.value?document.getElementById("spacer-group").style.display="none":document.getElementById("spacer-group").style.display="inline"})),l.addEventListener("change",(function(){document.getElementById("measurement-top").textContent=l.options[l.selectedIndex].value+'"',"1/16"==l.value?(document.getElementById("top-group").style.display="inline",document.getElementById("svg-top").setAttribute("points","103.5,61.3 40.7,61.3 40.7,65.3 109.2,65.3 ")):"1/8"==l.value?(document.getElementById("top-group").style.display="inline",document.getElementById("svg-top").setAttribute("points","103.5,61.3 40.7,61.3 40.7,69.3 109.2,69.3")):"0"==l.value&&(document.getElementById("top-group").style.display="none")}),!1),d.addEventListener("change",(function(){document.getElementById("measurement-bottom").textContent=d.options[d.selectedIndex].value+'"',"1/16"==d.value?(document.getElementById("svg-bottom").setAttribute("height","4"),document.getElementById("svg-bottom").style.display="inline",document.getElementById("measurement-bottom").style.display="inline"):"1/8"==d.value?(document.getElementById("svg-bottom").setAttribute("height","8"),document.getElementById("svg-bottom").style.display="inline",document.getElementById("measurement-bottom").style.display="inline"):"0"==d.value&&(document.getElementById("svg-bottom").style.display="none",document.getElementById("measurement-bottom").style.display="none")}),!1),i.addEventListener("input",(function(){document.getElementById("measurement-artwork").textContent=i.value+'"',0==i.value.length||"0"==i.value||""==i.value?document.getElementById("artwork-group").style.display="none":document.getElementById("artwork-group").style.display="inline"})),s.addEventListener("input",(function(){document.getElementById("measurement-backing").textContent=s.value+'"',0==s.value.length||"0"==s.value||""==s.value?(document.getElementById("svg-board").style.display="none",document.getElementById("measurement-backing").style.display="none"):(document.getElementById("svg-board").style.display="inline",document.getElementById("measurement-backing").style.display="inline")})),g.addEventListener("input",(function(){document.getElementById("measurement-additional").textContent=g.value+'"',0==g.value.length||0==g.value||""==g.value?document.getElementById("additional-group").style.display="none":document.getElementById("additional-group").style.display="inline"})),y.addEventListener("input",(function(){document.getElementById("measurement-strainer").textContent=y.value+'"',0==y.value.length||"0"==y.value||""==y.value?(document.getElementById("svg-strainer").style.display="none",document.getElementById("measurement-strainer").style.display="none"):(document.getElementById("svg-strainer").style.display="inline",document.getElementById("measurement-strainer").style.display="inline")})),p.addEventListener("input",(function(){document.getElementById("measurement-glazing").textContent=p.value+'"',"0"==p.value||0==p.length||""==p.value?(document.getElementById("svg-glazing").style.display="none",document.getElementById("measurement-glazing").style.display="none"):(document.getElementById("svg-glazing").style.display="inline",document.getElementById("measurement-glazing").style.display="inline")}));var h=function(e){if(parseFloat(e)===parseInt(e))return e;var t=function(e,n){return n<1e-7?e:t(n,Math.floor(e%n))},n=e.toString().length-2,a=Math.pow(10,n),l=e*a,o=t(l,a),d=0;return(l/=o)>(a/=o)&&(l-=(d=Math.floor(l/a))*a),e=Math.floor(l)+"/"+Math.floor(a),d&&(e=d+" "+e),e}}function downloadSVG(){var e=document.getElementById("download"),t=document.querySelector("svg");e.addEventListener("click",(function(){var e=document.getElementById("downloadable"),n=e.getContext("2d"),a=(new XMLSerializer).serializeToString(t),l=window.URL||window.webkitURL||window,o=new Image,d=new Blob([a],{type:"image/svg+xml;charset=utf-8"}),m=l.createObjectURL(d);o.onload=function(){n.clearRect(0,0,1800,2e3),n.drawImage(o,0,0,1800,2e3),l.revokeObjectURL(m),function(e){var t=new MouseEvent("click",{view:window,bubbles:!1,cancelable:!0}),n=document.createElement("a");n.setAttribute("download","frame-diagram.png"),n.setAttribute("href",e),n.setAttribute("target","_blank"),n.dispatchEvent(t)}(e.toDataURL("image/png").replace("image/png","image/octet-stream"))},o.src=m}))}function setYPosition(){var e,t;Array.from(document.getElementsByClassName("label")).forEach(n=>{e=n.previousElementSibling,t=parseFloat(e.getAttribute("y"));var a=parseFloat(e.getAttribute("height"))/2+t+1;n.setAttribute("y",a)})}window.addEventListener("load",eventWindowLoaded,!1);
+// )                )       )
+// ( /(   (  (      ( /(    ( /(   (
+// )\())  )\))(   ' )\())   )\())  )\ )
+// ((_)\  ((_)()\ ) ((_)\   ((_)\  (()/(
+// _((_) _(())\_)()  ((_)   _((_)  /(_))_
+// | \| | \ \((_)/ / / _ \  | \| | (_)) __|
+// | .` |  \ \/\/ / | (_) | | .` |   | (_ |
+// |_|\_|   \_/\_/   \___/  |_|\_|    \___|
+//
+// Created by Nathan Wong
+// https://nathanwong.studio
+//
+// Frame Depth Calculator v. 1.0
+// Copyright 2020 Nathan Wong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+window.addEventListener("load", eventWindowLoaded, false);
+
+function eventWindowLoaded() {
+	frameDesign();
+	downloadSVG();
+	setYPosition(); //Set the Y for all elements at start
+
+	//Adding Data- attribute to SVG elements with their intial heights
+
+	var svgRect = Array.from(document.getElementsByTagName("rect"))
+	;
+
+	svgRect.forEach((x) => {
+
+		var height = x.getAttribute('height');
+
+		x.setAttribute('data-height', height);
+
+	});
+
+}
+
+function frameDesign() {
+	var form = document.getElementById('frameCalc');
+	var remainder = document.getElementById('remaining');
+	var svgRemainder = document.getElementById('measurement-remainder');
+	var value;
+
+	form.addEventListener('change', calculate);
+	form.addEventListener('input', setVisibility);
+
+	var fields = document.getElementsByClassName("values");
+	var fieldArray = Array.from(fields);
+
+	fieldArray.forEach((x) => {
+		var height = x.getAttribute('height');
+
+		x.setAttribute('data-height', height);
+	});
+
+
+	//Reset the Form
+	var resetbtn = document.getElementById("resetButton");
+
+	resetbtn.addEventListener('click', function () {
+
+		// console.log(fields);
+
+		Array.from(fields, function(el){
+			el.value = 0;
+		});
+
+		calculate();
+		setVisibility();
+	});
+
+
+	//Calculator
+	//This function calculates the difference between the rabbet depth and the package depth and outputs a user-friendly fraction.
+
+	function calculate() {
+
+		//Reset any blank fields to 0
+		//
+		// Array.from(fields, function(el){
+		//
+		// 	if (el.value.length == 0 || el.value == 0) {
+		// 		el.value = 0;
+		// 	}
+		// });
+
+		// setting the height and positioning of everything based on the values given.
+		fieldArray.forEach((f) => {
+			value = f.value;
+
+			var newValue = fractionToNumber(value);
+
+			var svg = 'svg-' + f.id;
+			var group = 'not-' + f.id;
+			var elGroup = document.getElementById(group);
+			var el = document.getElementById(svg);
+			var height = parseFloat( el.getAttribute("data-height"));
+			var scaled = parseFloat(newValue) * 64;
+			var diff = scaled - height;
+
+			el.setAttribute("height", scaled);
+
+			if (f.id != "rabbet") { //ignore the rabbet
+				elGroup.setAttribute("transform", "translate(0 " + diff + ")");
+			}
+
+			if (f.id == "strainer") {
+				var x = document.getElementById('svg-x');
+				var initY = parseFloat(x.getAttribute('data-y'));
+				var newY = initY + diff;
+
+				x.setAttribute('y2', newY);
+			}
+
+		});
+
+		//Calculate the difference
+
+		var fracs = Array.from(fields);
+
+		fracs.forEach((f,i,fracs) => {
+			var frac = f.value;
+
+			fracs[i] = fractionToNumber(frac);
+		});
+
+		// console.log(fracs);
+
+		var sum = fracs.reduce(function(a, b){
+								return a + b;
+							}, 0);
+
+		var maths =(2 * fracs[0]) - sum;
+
+		remainder.value = numberToFraction(maths);
+		svgRemainder.textContent = remainder.value;
+
+		var fractioned;
+		var nonNeg;
+
+		if (maths < 0) {
+			nonNeg = Math.abs(maths)
+
+			fractioned = numberToFraction(nonNeg)
+
+			remainder.value = "-" + fractioned + "\42"
+			svgRemainder.textContent = "-" + fractioned + "\42"
+
+			remainder.classList.add('negative')
+
+			svgRemainder.setAttribute("class", 'negative')
+
+		} else {
+
+			fractioned = numberToFraction(maths)
+
+			remainder.value = fractioned + "\42"
+			svgRemainder.textContent = fractioned + "\42"
+
+			remainder.classList.remove('negative')
+
+			svgRemainder.setAttribute("class", 'positive')
+		}
+
+		setYPosition();
+	}
+
+	function setVisibility() {
+		fields = Array.from(fields);
+
+		fields.forEach((x) => {
+
+			var svgGroup = x.id + '-group';
+
+			// console.log(svgGroup);
+
+			var measurement = 'measurement-' + x.id;
+
+			document.getElementById(measurement).textContent = x.value + "\42";
+
+			if(x.value == 0 || x.value.length == 0) {
+				document.getElementById(svgGroup).style.display = "none";
+			} else {
+				document.getElementById(svgGroup).style.display = "inline";
+			}
+		});
+	}
+
+	/**
+	*
+	* Convert Fraction to Number
+	*
+	**/
+	var fractionToNumber = function(amount) {
+		if (amount.indexOf('/') != -1) {
+			var parts = amount.split(" ")
+			var decParts;
+			if (parts.length > 1) {
+				decParts = parts[1].split("/");
+			} else {
+				decParts = parts[0].split("/");
+				parts[0] = 0;
+			}
+			return parseInt(parts[0], 10) + (parseInt(decParts[0], 10) / parseInt(decParts[1], 10));
+		} else {
+			return parseInt(amount, 10)
+		}
+	}
+
+	/**
+	 * Converts numbers to fractions:
+	 * - 1.25 to 1 1/4
+	 * - 2 to 2
+	 */
+	var numberToFraction = function(amount) {
+		// This is a whole number and doesn't need modification.
+		if (parseFloat(amount) === parseInt(amount)) {
+			return amount;
+		}
+		// Next 12 lines are cribbed from https://stackoverflow.com/a/23575406.
+		var gcd = function(a, b) {
+			if (b < 0.0000001) {
+				return a;
+			}
+			return gcd(b, Math.floor(a % b));
+		};
+		var len = amount.toString().length - 2;
+		var denominator = Math.pow(10, len);
+		var numerator = amount * denominator;
+		var divisor = gcd(numerator, denominator);
+		numerator /= divisor;
+		denominator /= divisor;
+		var base = 0;
+		// In a scenario like 3/2, convert to 1 1/2
+		// by pulling out the base number and reducing the numerator.
+		if (numerator > denominator) {
+			base = Math.floor(numerator / denominator);
+			numerator -= base * denominator;
+		}
+		amount = Math.floor(numerator) + '/' + Math.floor(denominator);
+		if (base) {
+			amount = base + ' ' + amount;
+		}
+		return amount;
+	};
+}
+
+//add art depth
+//add svg to png
+
+function downloadSVG() {
+	var btn = document.getElementById('download');
+	var svg = document.querySelector('svg');
+
+	function triggerDownload (imgURI) {
+		var evt = new MouseEvent('click', {
+		  view: window,
+		  bubbles: false,
+		  cancelable: true
+		});
+
+		var a = document.createElement('a');
+		a.setAttribute('download', 'frame-diagram.png');
+		a.setAttribute('href', imgURI);
+		a.setAttribute('target', '_blank');
+
+		a.dispatchEvent(evt);
+		}
+
+		btn.addEventListener('click', function () {
+				var canvas = document.getElementById('downloadable');
+				var ctx = canvas.getContext('2d');
+				var data = (new XMLSerializer()).serializeToString(svg);
+				var DOMURL = window.URL || window.webkitURL || window;
+
+				var img = new Image();
+				var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+				var url = DOMURL.createObjectURL(svgBlob);
+
+				img.onload = function () {
+						ctx.clearRect(0, 0, 1800, 2000);
+						ctx.drawImage(img, 0, 0, 1800, 2000);
+				  DOMURL.revokeObjectURL(url);
+
+				  var imgURI = canvas
+				      .toDataURL('image/png')
+				      .replace('image/png', 'image/octet-stream');
+
+				  triggerDownload(imgURI);
+				};
+
+				img.src = url;
+		});
+}
+function setYPosition() {
+	var label = Array.from(document.getElementsByClassName("label"));
+	var sibling;
+	var height;
+	var y;
+
+	// console.log(document.getElementById("svg-strainer"))
+
+	label.forEach((el) => {
+			sibling = el.previousElementSibling;
+
+			y = parseFloat(sibling.getAttribute("y"))
+			height = parseFloat(sibling.getAttribute("height"));
+
+			var yPosition = height / 2 + y + 1;
+
+			el.setAttribute("y", yPosition);
+
+			// console.log(el.id + y + " " + height);
+
+
+	});
+
+
+
+}
