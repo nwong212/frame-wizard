261b8c148823bfcd5f3066953b02797ec5c6b3b7
Nathan <nwong@risd.edu>
2020-08-07 16:42:34 -0400

    replaced service-worker with workbox

--- source/service-worker.js
+++ source/service-worker.js
@@ -1,68 +1,78 @@
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+const {precacheAndRoute} = workbox.precaching;
+
+precacheAndRoute([
+  {url: '/index.html', revision: '1' },
+  {url: '/assets/styles/styles.css', revision: null},
+  {url: '/assets/scripts/wizard.js', revision: null},
+  // ... other entries ...
+]);
+
 //
-// if (workbox) {
-//   console.log(`Yay! Workbox is loaded 🎉`);
-// } else {
-//   console.log(`Boo! Workbox didn't load 😬`);
-// }
-
-var APP_PREFIX = 'FrameDepth_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_02'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/assets/scripts/wizard.js',                     // If you have separate JS/CSS files,
-	'/assets/styles/styles.css',
-	'/assets/icons/X.svg',
-  '/index.html'            // add path to those files here
-]
-
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
-
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
-})
+// var APP_PREFIX = 'FrameDepth_'     // Identifier for this app (this needs to be consistent across every cache update)
+// var VERSION = 'version_02'              // Version of the off-line cache (change this value everytime you want to update cache)
+// var CACHE_NAME = APP_PREFIX + VERSION
+// var URLS = [                            // Add URL you want to cache in this list.
+//   '/assets/scripts/wizard.js',                     // If you have separate JS/CSS files,
+// 	'/assets/styles/styles.css',
+// 	'/assets/icons/X.svg',
+//   '/index.html'            // add path to those files here
+// ]
+//
+// // Respond with cached resources
+// self.addEventListener('fetch', function (e) {
+//   console.log('fetch request : ' + e.request.url)
+//   e.respondWith(
+//     caches.match(e.request).then(function (request) {
+//       if (request) { // if cache is available, respond with cache
+//         console.log('responding with cache : ' + e.request.url)
+//         return request
+//       } else {       // if there are no cache, try fetching request
+//         console.log('file is not cached, fetching : ' + e.request.url)
+//         return fetch(e.request)
+//       }
+//
+//       // You can omit if/else for console.log & put one line below like this too.
+//       // return request || fetch(e.request)
+//     })
+//   )
+// })
+//
+// // Cache resources
+// self.addEventListener('install', function (e) {
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log('installing cache : ' + CACHE_NAME)
+//       return cache.addAll(URLS)
+//     })
+//   )
+// })
+//
+// // Delete outdated caches
+// self.addEventListener('activate', function (e) {
+//   e.waitUntil(
+//     caches.keys().then(function (keyList) {
+//       // `keyList` contains all cache names under your username.github.io
+//       // filter out ones that has this app prefix to create white list
+//       var cacheWhitelist = keyList.filter(function (key) {
+//         return key.indexOf(APP_PREFIX)
+//       })
+//       // add current cache name to white list
+//       cacheWhitelist.push(CACHE_NAME)
+//
+//       return Promise.all(keyList.map(function (key, i) {
+//         if (cacheWhitelist.indexOf(key) === -1) {
+//           console.log('deleting cache : ' + keyList[i] )
+//           return caches.delete(keyList[i])
+//         }
+//       }))
+//     })
+//   )
+// })
